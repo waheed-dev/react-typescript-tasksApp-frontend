17341bd1e4d24a2fb775703a60831036c4752a6b
root <root@DESKTOP-JFOC4RH>
2023-01-17 10:54:58 +0500

    add custom theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import { useState } from 'react'
+import { useState, FC, ReactElement,} from 'react'
+import {ThemeProvider, CssBaseline} from '@mui/material'
+import {theme} from './theme/theme'
 
 
-const App = () => {
+const App : FC = () :ReactElement => {
     return (
-        <div>hello world</div>
+        <ThemeProvider theme={theme}>
+            <CssBaseline/>
+            <div>hello worssssld</div>
+        </ThemeProvider>
+
     )
 }
 
