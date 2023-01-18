733997e9ab147ce9b97e33c4d7d044347c39dfff
Waheed <a.waheed3738@gmail.com>
2023-01-18 18:06:16 +0500

    add layout for task area

--- src/components/taskArea/TaskArea.tsx
+++ src/components/taskArea/TaskArea.tsx
@@ -1,10 +1,31 @@
 import {FC, ReactElement} from "react";
-import {Grid} from '@mui/material'
-
+import {Grid,Box} from '@mui/material'
+import {format} from 'date-fns'
 const TaskArea : FC = () : ReactElement => {
   return (
       <Grid item md={8} px={4}>
-          <h2>conten Area</h2>
+          <Box mb={8} px={4}>
+              <h2>Status of your tasks as of {format(new Date(),'PPPP')}</h2>
+          </Box>
+          <Grid container display={'flex'} justifyContent={'center'}>
+              <Grid item
+                    display={'flex'}
+                    flexDirection={'row'}
+                    justifyContent={'space-around'}
+                    alignItems={'center'}
+                    md={10}
+                    xs={12}
+                    mb={8}
+              >
+                  <Box>Task counter</Box>
+                  <Box>Task counter</Box>
+                  <Box>Task counter</Box>
+              </Grid>
+              <Grid item display={'flex'} flexDirection={'column'} xs={10} md={8}>
+                  <Box>Tasks</Box>
+                  <Box>Tasks</Box>
+              </Grid>
+          </Grid>
       </Grid>
   )
 }
